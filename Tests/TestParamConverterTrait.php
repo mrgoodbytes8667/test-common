28f06d8de7ca55d2d5d72f240542ae1f2af0b219
MrGoodBytes <bytes@goodbytes.live>
2021-03-23 10:22:25 -0500

    Added a ParamConverter helper trait

---
+++ Tests/TestParamConverterTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Bytes\Tests\Common;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+/**
+ * Trait TestParamConverterTrait
+ * @package Bytes\Tests\Common
+ *
+ * Based on the SensioFrameworkBundle ParamConverter tests
+ * @link https://github.com/sensiolabs/SensioFrameworkExtraBundle/blob/89d6f6218406d8c62349f0a28706563755332af0/src/Request/ParamConverter/DateTimeParamConverter.php
+ */
+trait TestParamConverterTrait
+{
+    /**
+     * @param null $class
+     * @param null $name
+     * @param bool $optional
+     * @return ParamConverter
+     */
+    public function createConfiguration($class = null, $name = null, bool $optional = false)
+    {
+        $config = $this
+            ->getMockBuilder(ParamConverter::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        if (null !== $name) {
+            $config->expects($this->any())
+                ->method('getName')
+                ->willReturn($name);
+        }
+        if (null !== $class) {
+            $config->expects($this->any())
+                ->method('getClass')
+                ->willReturn($class);
+        }
+        $config->method('isOptional')
+            ->willReturn($optional);
+
+        return $config;
+    }
+}
