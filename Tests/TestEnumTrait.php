65c2cc4e5f1def2bc62f1374f981942fc7bc1e05
MrGoodBytes <bytes@mrgoodbytes.dev>
2023-04-12 11:32:30 -0500

    Drop support for PHP 8.0, require newer enum bundle

--- Tests/TestEnumTrait.php
+++ Tests/TestEnumTrait.php
@@ -2,9 +2,8 @@
 
 namespace Bytes\Tests\Common;
 
-use Bytes\EnumSerializerBundle\Enums\Enum;
-use ReflectionClass;
-use ReflectionException;
+use BackedEnum;
+use JetBrains\PhpStorm\Deprecated;
 
 
 /**
@@ -17,33 +16,20 @@ trait TestEnumTrait
 {
     /**
      * Returns an array of all docblock-defined enum methods hydrated into instances of the class
-     * @param object|string $enum A class name or instance of the Enum class
-     * @return Enum[]
-     * @throws ReflectionException
+     * @param BackedEnum|class-string<BackedEnum> $enum A class name or instance of the Enum class
+     * @return BackedEnum[]
      */
-    public static function extractAllFromEnum($enum)
+    #[Deprecated('Since 0.1.0, use the enum cases() method instead', '%class%::cases()')]
+    public static function extractAllFromEnum(BackedEnum|string $enum)
     {
-        $reflectionClass = new ReflectionClass($enum);
-
-        $docComment = $reflectionClass->getDocComment();
-
-        preg_match_all('/@method\s+static\s+self\s+([\w_]+)\(\)/', $docComment, $matches);
-
-        $definition = [];
-
-        foreach ($matches[1] as $methodName) {
-            $definition[] = $enum::make($methodName);
-        }
-
-        return $definition;
+        return $enum::cases();
     }
 
     /**
-     * @param Enum|string $class
+     * @param BackedEnum|string $class
      */
-    public function coverEnum(string $class)
+    public function coverEnum(BackedEnum|string $class)
     {
-        $this->assertIsArray($class::toValues());
-        $this->assertIsArray($class::toLabels());
+        $this->assertIsArray($class::values());
     }
 }
